2cf1b3e874e6d65e07d03e2f162fef386a73f863
Arthur Mensch <arthur.mensch@gmail.com>
2020-09-01 11:00:45 +0200

    new slides

---
+++ notebooks/02_sklearn/solutions/06-Pipeline-1.py
@@ -0,0 +1,6 @@
+# Define a pipeline to search for the best combination of PCA truncation
+# and classifier regularization.
+pca = PCA()
+# set the tolerance to a large value to make the example faster
+logistic = LogisticRegression(max_iter=10000, tol=0.1)
+pipe = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
